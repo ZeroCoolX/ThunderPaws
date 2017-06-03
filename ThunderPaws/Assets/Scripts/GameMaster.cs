e6b86c46d446c850e80178f1977b5d430567b347
cnpatton <botanicality@gmail.com>
2017-06-03 14:32:33 -0500

    Added weapon switching

--- ThunderPaws/Assets/Scripts/GameMaster.cs
+++ ThunderPaws/Assets/Scripts/GameMaster.cs
@@ -8,6 +8,15 @@ public class GameMaster : MonoBehaviour {
     //Camera instance
     public CameraShake camShake;
 
+    //weapon choice 1, 2,...etc default 1 (pistol)
+    [SerializeField]
+    private int _weaponChoice = 0;
+    public int weaponChoice { get { return _weaponChoice; } set { _weaponChoice = value; } }
+
+    //Delegate for switching weapons
+    public delegate void WeaponSwitchCallback(int choice);
+    public WeaponSwitchCallback onWeaponSwitch;
+
     private void Awake() {
         if(instance == null) {
             instance = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
@@ -19,4 +28,18 @@ public class GameMaster : MonoBehaviour {
             Debug.LogError("GameMaster.cs: No CameraShake found");
         }
     }
+
+    private void Update() {
+        //if the user is switching weapons, change the selection, then update the delegate so the player knows to switch
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            //switch to pistol
+            weaponChoice = 1;
+            onWeaponSwitch.Invoke(weaponChoice);
+        }else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            //switch to machine gun
+            weaponChoice = 2;
+            onWeaponSwitch.Invoke(weaponChoice);
+        }
+    }
+
 }
---
+++ ThunderPaws/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+[RequireComponent(typeof(Platformer2DUserControl))]
+public class Player : MonoBehaviour {
+    //TODO: health and player stats
+
+    //weapon refeerences to enable/disable based on user input
+    [SerializeField]
+    private GameObject _machineGun; //2
+    [SerializeField]
+    private GameObject _pistol; //1
+
+    private void Start() {
+        //select the default weapon
+        SelectWeapon(GameMaster.instance.weaponChoice);
+        //Add the weapon switch method onto the weaponSwitch delegate
+        GameMaster.instance.onWeaponSwitch += SelectWeapon;
+    }
+
+    private void SelectWeapon(int choice) {
+        //TODO: only allow weapon switch if they have the weapon
+        if(_machineGun != null && _pistol != null) {
+            _machineGun.SetActive(false);
+            _pistol.SetActive(false);
+            switch (choice) {
+                case 1:
+                    _pistol.SetActive(true);
+                    break;
+                case 2:
+                    _machineGun.SetActive(true);
+                    break;
+                default:
+                    _pistol.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
