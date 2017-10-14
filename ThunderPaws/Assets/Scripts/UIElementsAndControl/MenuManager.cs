91c2e4f832c29773f87241f5ec911744029d88ce
cnpatton <botanicality@gmail.com>
2017-10-14 04:10:48 -0500

    Added a main menu with play and quit. Added nifty little animation to the menu, and scene loading to scene where you can play a version of the game that is fun

---
+++ ThunderPaws/Assets/Scripts/UIElementsAndControl/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public string sceneToLoadOnPlay = "CollisionPlayground";
+
+    public void StartGame() {
+        SceneManager.LoadScene(sceneToLoadOnPlay);
+    }
+
+    public void QuitGame() {
+        print("Quit game");
+        Application.Quit();
+    }
+}
